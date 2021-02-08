76ee5a8ef800820b385c7dd83fbaa4fccab4f240
Steven Sussman <sjsussman@gmail.com>
2021-02-08 14:55:20 -0500

    added intro

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
+import "../styles/home.css";
 
 const Home = () => {
+  return (
+    <div>
+      <h1>Hello, my name is Steven Sussman</h1>
+      <p>
+        I am a full stack web developer with an enthusiasm for UX/UI design.
+      </p>
+      <p>Check out my work here.</p>
+      <p>Want to chat? You can reach me here.</p>
+      <p>While you're here, take a look at my tech stack!</p>
+    </div>
+  );
+};
 
-    return(
-        <h1>Test</h1>
-    )
-}
-
-export default Home
+export default Home;
