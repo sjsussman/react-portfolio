8763ef8b7047e5d564a24898daeb7b172cdd8e1d
Steven Sussman <sjsussman@gmail.com>
2021-02-09 12:32:50 -0500

    added contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from "react";
+import "../styles/contact.css";
 
 const Contact = () => {
+  return (
+    <nav className="social">
+      <a
+        href="http://github.com/sjsussman"
+        target="_blank"
+        style={{ fontSize: "10rem", color: "black" }}
+      >
+        <i class="fab fa-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/steven-sussman/"
+        target="_blank"
+        style={{ fontSize: "10rem", color: "black" }}
+      >
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+      <a
+        href="http://twitter.com/namssus"
+        target="_blank"
+        style={{ fontSize: "10rem", color: "black" }}
+      >
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a
+        href="mailto:hello@sjsussman.com"
+        target="_blank"
+        style={{ fontSize: "10rem", color: "black" }}
+      >
+        <i class="fas fa-at"></i>
+      </a>
+    </nav>
+  );
+};
 
-    return(
-        <h1>Test</h1>
-    )
-}
-
-export default Contact
+export default Contact;
