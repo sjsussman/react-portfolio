5d0dc258204a50bc0ce58a899e101c052b52f135
Steven Sussman <sjsussman@gmail.com>
2021-02-09 09:50:51 -0500

    added spring cards and styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/NavBar/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
       <Switch>
-        <Route path="contact">
+        <Route path="/contact">
           <Contact />
         </Route>
-        <Route path="contact">
+        <Route path="/projects">
           <Projects />
         </Route>
         <Route path="/">
