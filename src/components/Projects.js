02f4107a3cd043018f8c1b37d0824307fae66d38
Steven Sussman <sjsussman@gmail.com>
2021-02-09 17:28:13 -0500

    added projects, updated styling and favicon

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,26 @@
 import React from "react";
 import Card from "./Project Card/Card.js";
+import "../styles/project.css";
 
 const Projects = () => {
   return (
     <div>
-      <Card />
+      <div id="project-banner">
+        <h1>Just a piece of advice...</h1>
+        <p>Click on a card to learn more about the project</p>
+      </div>
+      <div id="cardWrapper">
+        <Card />
+      </div>
+      <div id="link-container">
+        <a
+          href="https://my-dev-chat.vercel.app/"
+          target="_blank"
+          id="devAppLink"
+        >
+          Check out the deployed site
+        </a>
+      </div>
     </div>
   );
 };
